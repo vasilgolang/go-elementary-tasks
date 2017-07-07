9f50db93eb57f6751c69f973a6536bb8e861f958
VasilSV <vastzp@gmail.com>
2017-07-07 11:36:38 +0300

    Emplemented run all

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -6,15 +6,17 @@ import (
 	"fmt"
 	"encoding/json"
 	"io/ioutil"
-	"github.com/vasilgolang/go-elementary-tasks/task2"
 	"strconv"
 	"github.com/vasilgolang/go-elementary-tasks/taskmanager"
+	"strings"
 )
 
-func handlerMainPage(w http.ResponseWriter, r *http.Request) {
-	//content, _ := ioutil.ReadFile(`static/index.html`)
-	//w.Write(content)
+type TaskResult struct {
+	Err    string
+	Result string
+}
 
+func handlerMainPage(w http.ResponseWriter, r *http.Request) {
 	// redirect to static index.html
 	http.Redirect(w, r, `/static/index.html`, http.StatusSeeOther)
 }
@@ -22,20 +24,36 @@ func handlerMainPage(w http.ResponseWriter, r *http.Request) {
 func handlerAllTasks(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Errorf("ioutil.ReadAll(r.Body) Error:", err)
+		fmt.Errorf("ioutil.ReadAll(r.Body) Error:%s", err)
 	}
-	fmt.Println("body:", string(body))
-	var t task2.Params
-
-	err = json.Unmarshal(body, &t)
-	if err != nil {
-		fmt.Errorf("json.Unmarshal(body, t) Error:", err)
-	}
+	//fmt.Println("body:", string(body))
 	defer r.Body.Close()
-	fmt.Printf("Values: %#v\r\n", t)
-	res, minEnvelope, err := task2.CanEncloseEnvelopes(t.Envelope1, t.Envelope2)
-	fmt.Println(res, minEnvelope, err)
-	w.Write([]byte(fmt.Sprintf("res:%v, minEnvelope:%v, err:%v", res, minEnvelope, err)))
+
+	params := map[string]json.RawMessage{}
+	err = json.Unmarshal([]byte(body), &params)
+	if err != nil {
+		fmt.Errorf("Unmarshal Error:", err)
+	}
+	results := map[string]TaskResult{}
+	for k, v := range params {
+		sNumber := strings.Replace(k, `task`, ``, 1)
+		if i, err := strconv.Atoi(sNumber); err != nil {
+			fmt.Println("Error:", err)
+			continue
+		} else {
+			result, err := taskmanager.RunTask(i, string(v))
+			results[k] = TaskResult{
+				Result: result,
+				Err:    ErrToString(err),
+			}
+		}
+
+	}
+
+	fmt.Printf("Results:\r\n%#v\r\n", results)
+	b, _:= json.Marshal(results)
+	//fmt.Println(err, string(b))
+	w.Write(b)
 }
 
 func handlerTask(w http.ResponseWriter, r *http.Request) {
@@ -50,16 +68,12 @@ func handlerTask(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Errorf("ioutil.ReadAll(r.Body) Error:", err).Error()))
-
+		w.Write([]byte(fmt.Errorf("ioutil.ReadAll(r.Body) Error: %v", err).Error()))
 	}
 
 	result, err := taskmanager.RunTask(taskNumber, string(body))
 
-	b, _ := json.Marshal(struct {
-		Err    string
-		Result string
-	}{
+	b, _ := json.Marshal(TaskResult{
 		Err:    ErrToString(err),
 		Result: result,
 	})
@@ -71,9 +85,9 @@ func Run() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", handlerMainPage) // set router
-	//http.HandleFunc("/tasks/all", handlerAllTasks) // set router
-	http.HandleFunc("/task/", handlerTask) // set router
+	http.HandleFunc("/", handlerMainPage)       // set router
+	http.HandleFunc("/task/", handlerTask)      // set router
+	http.HandleFunc("/tasks/", handlerAllTasks) // set router
 
 	err := http.ListenAndServe(":9090", nil) // set listen port
 	if err != nil {
